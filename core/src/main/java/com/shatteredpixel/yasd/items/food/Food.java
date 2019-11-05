7df7abe00d309c26b059537dc99541fc0553e4af
samue <smujamesb@gmail.com>
2019-11-05 13:22:13 +0000

    Fixes

--- core/src/main/java/com/shatteredpixel/yasd/items/food/Food.java
+++ core/src/main/java/com/shatteredpixel/yasd/items/food/Food.java
@@ -92,7 +92,7 @@ public class Food extends Item {
 	
 	protected void satisfy( Hero hero ){//Eating food satisfies morale as well as hunger.
 		Buff.affect(hero, Hunger.class).satisfy( energy );
-		hero.gainMorale(2f);
+		hero.gainMorale(0.5f);
 	}
 	
 	public static void foodProc( Hero hero ){
