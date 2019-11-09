cd1e80f7a3e77dff764f8de26aba6bda1f5934e7
samue <smujamesb@gmail.com>
2019-11-09 14:42:19 +0000

    Refactored flying

--- core/src/main/java/com/shatteredpixel/yasd/levels/traps/GrippingTrap.java
+++ core/src/main/java/com/shatteredpixel/yasd/levels/traps/GrippingTrap.java
@@ -56,7 +56,7 @@ public class GrippingTrap extends Trap {
 
 		Char c = Actor.findChar( pos );
 
-		if (c != null && !c.flying) {
+		if (c != null && !c.isFlying()) {
 			int damage = Math.max( 0,  (2 + Dungeon.depth) - c.drRoll() );
 			Buff.affect( c, Bleeding.class ).set( damage );
 			Buff.prolong( c, Cripple.class, Cripple.DURATION);
