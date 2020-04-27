c3961a16a7aadc1c6572830b09b739362d114a9d
samue <smujamesb@gmail.com>
2020-04-27 14:28:58 +0100

    Porting v0.8.0: tweaked new DM abilities to interrupt the hero

--- core/src/main/java/com/shatteredpixel/yasd/general/actors/mobs/NewDM300.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/actors/mobs/NewDM300.java
@@ -306,6 +306,7 @@ public class NewDM300 extends Mob {
 	}
 
 	public void ventGas( Char target ){
+		Dungeon.hero.interrupt();
 
 		int gasVented = 0;
 
@@ -334,6 +335,7 @@ public class NewDM300 extends Mob {
 	}
 
 	public void dropRocks( Char target ) {
+		Dungeon.hero.interrupt();
 
 		int rockCenter = target.pos;
 
