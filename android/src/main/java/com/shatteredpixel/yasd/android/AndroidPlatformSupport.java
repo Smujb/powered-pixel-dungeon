5f3a044406771660eed4c406d2b6c6c75819df9f
samue <smujamesb@gmail.com>
2020-06-06 12:27:31 +0100

    Fixed broken landscape mode switching.

--- android/src/main/java/com/shatteredpixel/yasd/android/AndroidPlatformSupport.java
+++ android/src/main/java/com/shatteredpixel/yasd/android/AndroidPlatformSupport.java
@@ -71,8 +71,9 @@ public class AndroidPlatformSupport extends PlatformSupport {
 	}
 	
 	public void updateDisplaySize(){
-		if (YASDSettings.landscape() != null) {
-			AndroidGame.instance.setRequestedOrientation( PixelScene.landscape() ?
+		Boolean landscape = YASDSettings.landscape();
+		if (landscape != null) {
+			AndroidGame.instance.setRequestedOrientation( landscape ?
 					ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE :
 					ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT );
 		}
@@ -86,8 +87,6 @@ public class AndroidPlatformSupport extends PlatformSupport {
 
 		boolean fullscreen = Build.VERSION.SDK_INT < Build.VERSION_CODES.N
 				|| !AndroidGame.instance.isInMultiWindowMode();
-
-		Boolean landscape = YASDSettings.landscape();
 
 		if (fullscreen && landscape != null
 				&& (Game.dispWidth >= Game.dispHeight) != landscape) {
