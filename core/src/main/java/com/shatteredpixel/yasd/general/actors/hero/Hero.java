c4c74a89942a62262469ed8df2b2c3fe545ed50d
samue <smujamesb@gmail.com>
2020-06-26 16:26:38 +0100

    Fixed no death message from many blobs.

--- core/src/main/java/com/shatteredpixel/yasd/general/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/actors/hero/Hero.java
@@ -1440,9 +1440,9 @@ public class Hero extends Char {
 		}
 
 		GameScene.gameOver();
-		
-		if (cause instanceof Hero.Doom) {
-			((Hero.Doom)cause).onDeath();
+
+		if (cause.getCause() instanceof Hero.Doom) {
+			((Hero.Doom)cause.getCause()).onDeath();
 		}
 		
 		Dungeon.deleteGame( GamesInProgress.curSlot, true );
