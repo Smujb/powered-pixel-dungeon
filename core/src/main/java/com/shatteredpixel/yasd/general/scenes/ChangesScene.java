215615f9ef55d7442ac528bf069d417281bded3a
samue <smujamesb@gmail.com>
2020-07-15 15:38:20 +0100

    Added V0.8.X Shattered changelog.

--- core/src/main/java/com/shatteredpixel/yasd/general/scenes/ChangesScene.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/scenes/ChangesScene.java
@@ -46,6 +46,7 @@ import com.shatteredpixel.yasd.general.ui.changelist.v0_4_X_Changes;
 import com.shatteredpixel.yasd.general.ui.changelist.v0_5_X_Changes;
 import com.shatteredpixel.yasd.general.ui.changelist.v0_6_X_Changes;
 import com.shatteredpixel.yasd.general.ui.changelist.v0_7_X_Changes;
+import com.shatteredpixel.yasd.general.ui.changelist.v0_8_X_Changes;
 import com.watabou.noosa.Camera;
 import com.watabou.noosa.NinePatch;
 import com.watabou.noosa.audio.Music;
@@ -97,6 +98,7 @@ public class ChangesScene extends PixelScene {
 				YASD_log.addAllChanges(changeInfos);
 				break;
 			case 1:
+				v0_8_X_Changes.addAllChanges(changeInfos);
 				v0_7_X_Changes.addAllChanges(changeInfos);
 				v0_6_X_Changes.addAllChanges(changeInfos);
 				v0_5_X_Changes.addAllChanges(changeInfos);
