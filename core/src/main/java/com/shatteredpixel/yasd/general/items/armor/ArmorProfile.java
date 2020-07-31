41f534e8ad1356297e7fbaf565675db5be757e92
samue <smujamesb@gmail.com>
2020-07-31 13:00:15 +0100

    Fixed Ethereal Armour applying to mainly Physical armours.

--- core/src/main/java/com/shatteredpixel/yasd/general/items/armor/ArmorProfile.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/items/armor/ArmorProfile.java
@@ -120,14 +120,14 @@ public enum ArmorProfile implements Profile {
 			case EVASION_DECREASE:
 				return 1/armor.EVA;
 			case PHYSICAL:
-				if (armor.physicalResist > 1.2f) {
-					return armor.physicalResist;
+				if (armor.physicalResist < 0.8f) {
+					return 1/armor.physicalResist;
 				} else {
 					return 1f;
 				}
 			case MAGICAL:
-				if (armor.magicalResist > 1.2f) {
-					return armor.magicalResist;
+				if (armor.magicalResist < 0.8f) {
+					return 1/armor.magicalResist;
 				} else {
 					return 1f;
 				}
