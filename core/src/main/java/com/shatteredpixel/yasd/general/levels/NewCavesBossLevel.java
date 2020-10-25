2acfd1d4603a177849f0318207276e4e4c588910
Samuel Braithwaite <smujamesb@gmail.com>
2020-10-25 14:42:27 +0000

    Fixed 0 HP Pylons for real

--- core/src/main/java/com/shatteredpixel/yasd/general/levels/NewCavesBossLevel.java
+++ core/src/main/java/com/shatteredpixel/yasd/general/levels/NewCavesBossLevel.java
@@ -202,7 +202,7 @@ public class NewCavesBossLevel extends Level {
 
 			for (int i : pylonPositions) {
 				if (findMob(i) == null) {
-					Pylon pylon = new Pylon();
+					Pylon pylon = Mob.create(Pylon.class, this);
 					pylon.pos = i;
 					mobs.add(pylon);
 				}
@@ -214,7 +214,7 @@ public class NewCavesBossLevel extends Level {
 	@Override
 	protected void createMobs() {
 		for (int i : pylonPositions) {
-			Pylon pylon = new Pylon();
+			Pylon pylon = Mob.create(Pylon.class, this);
 			pylon.pos = i;
 			mobs.add(pylon);
 		}
